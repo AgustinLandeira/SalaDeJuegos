34cc38d133c2063f0f7abc3f254d2539e37df654
agus2023 <landeira2017@gmail.com>
2024-10-25 18:15:42 -0300

    realizando login

---
+++ src/enviromentConfig.ts
@@ -0,0 +1,9 @@
+export const enviroment = {
+
+    apiKey: "AIzaSyByimqYgzMRjr5IKpr9N-SIoF_1e-xzWnE",
+    authDomain: "sala-de-juegos-1c9ea.firebaseapp.com",
+    projectId: "sala-de-juegos-1c9ea",
+    storageBucket: "sala-de-juegos-1c9ea.appspot.com",
+    messagingSenderId: "280351401243",
+    appId: "1:280351401243:web:681406c8319541736e1fbe"
+}

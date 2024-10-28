1b13292c2544be52431261667cfa8eac94faa0a3
agus2023 <landeira2017@gmail.com>
2024-10-28 19:42:01 -0300

    login completado

--- src/app/componentes/register/register.component.ts
+++ src/app/componentes/register/register.component.ts
@@ -2,6 +2,7 @@ import { Component, NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { Router, RouterLink,RouterLinkActive } from '@angular/router';
 import { Auth, createUserWithEmailAndPassword } from '@angular/fire/auth';
+import Swal from 'sweetalert2';
 @Component({
   selector: 'app-register',
   standalone: true,
@@ -15,15 +16,16 @@ export class RegisterComponent {
   public registrado! : boolean;
   public msjError : string = "";
 
-  constructor(router:Router,public auth:Auth){}
+  constructor(public router:Router,public auth:Auth){}
 
   Registrar(){
 
     createUserWithEmailAndPassword(this.auth,this.mail,this.clave).then((res) =>{
 
       if(res.user.email != null){
-        this.registrado = true;
-        console.log(this.registrado);
+        
+        this.showSuccessAlert("Te logeamos automaticamente");
+        this.router.navigate(["home"]);
       }
       
     }).catch((e) =>{
@@ -52,5 +54,17 @@ export class RegisterComponent {
 
       }
     });//
+
+  }
+  private showSuccessAlert(message: string) {
+    return Swal.fire({
+      title: 'Registro exitoso',
+      text: message,
+      icon: 'success',
+      confirmButtonText: 'OK',
+      background : "black",
+      color: "green",
+      confirmButtonColor: "green"
+    });
   }
 }

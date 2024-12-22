e577fd66b17f7fb9b96d35053c1894a1a35b74ad
agus2023 <landeira2017@gmail.com>
2024-12-22 17:43:39 -0300

    haciendo el page not found

--- src/app/componentes/page-not-found/page-not-found.component.ts
+++ src/app/componentes/page-not-found/page-not-found.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-page-not-found',
@@ -8,5 +9,12 @@ import { Component } from '@angular/core';
   styleUrl: './page-not-found.component.css'
 })
 export class PageNotFoundComponent {
+
+  public constructor( public route: Router){}
+
+  public volver(){
+    
+    this.route.navigate(["home"]);
+  }
 
 }
